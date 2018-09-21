a99a1d2d5654fdd25f9b6bc125f81c2ba6dcfe0c
FabienCOTE <fabien.cote@gmail.com>
2018-09-21 14:17:29 +0200

    Fin du menu

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function myResponsiveNavbar() {
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
